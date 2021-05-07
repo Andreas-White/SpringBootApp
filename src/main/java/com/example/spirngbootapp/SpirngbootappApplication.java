b0d691bd724f1a15db4f1658e82bfabcda735016
Andreas Manos <antmanos42@gmail.com>
2021-05-07 03:21:13 +0200

    starting Spring Boot App

---
+++ src/main/java/com/example/spirngbootapp/SpirngbootappApplication.java
@@ -0,0 +1,13 @@
+package com.example.spirngbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpirngbootappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpirngbootappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spirngbootapp/SpirngbootappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spirngbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpirngbootappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
