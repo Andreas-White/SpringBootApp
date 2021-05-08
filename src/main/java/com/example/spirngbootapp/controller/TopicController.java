66bba0e9ab3f46675bb3014a49f19b9178e10339
Andreas Manos <antmanos42@gmail.com>
2021-05-08 02:59:19 +0200

    implementing TopicController with RestController and RequestMapping that returns a List of Topic objects as a JSON

--- src/main/java/com/example/spirngbootapp/controller/TopicController.java
+++ src/main/java/com/example/spirngbootapp/controller/TopicController.java
@@ -1,13 +1,21 @@
 package com.example.spirngbootapp.controller;
 
+import com.example.spirngbootapp.model.Topic;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 public class TopicController {
 
     @RequestMapping("/topics")
-    public String getAllTopics() {
-        return "All topics";
+    public List<Topic> getAllTopics() {
+        return Arrays.asList(
+                new Topic("spring","Spring Framework","Spring Framework Description"),
+                new Topic("java","Core Java language","Core java language description"),
+                new Topic("javascript","Core JavaScript language","Core javascript language description")
+        );
     }
 }
