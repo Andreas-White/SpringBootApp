d1fce532d1e8b926c0b5ad0fe3a0ccb596005908
Andreas Manos <antmanos42@gmail.com>
2021-05-09 02:17:16 +0200

    initialising TopicController to TopicService class

--- src/main/java/com/example/spirngbootapp/businessService/TopicService.java
+++ src/main/java/com/example/spirngbootapp/businessService/TopicService.java
@@ -1,6 +1,22 @@
 package com.example.spirngbootapp.businessService;
 
+import com.example.spirngbootapp.model.Topic;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
 public class TopicService {
 
-    
+    private List<Topic> topics = Arrays.asList(
+            new Topic("spring","Spring Framework","Spring Framework Description"),
+            new Topic("java","Core Java language","Core java language description"),
+            new Topic("javascript","Core JavaScript language","Core javascript language description")
+    );
+
+    public List<Topic> getTopics() {
+        return topics;
+    }
 }
--- src/main/java/com/example/spirngbootapp/controller/TopicController.java
+++ src/main/java/com/example/spirngbootapp/controller/TopicController.java
@@ -1,6 +1,8 @@
 package com.example.spirngbootapp.controller;
 
+import com.example.spirngbootapp.businessService.TopicService;
 import com.example.spirngbootapp.model.Topic;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,12 +12,19 @@ import java.util.List;
 @RestController
 public class TopicController {
 
+    private TopicService service;
+
+    public TopicService getService() {
+        return service;
+    }
+
+    @Autowired
+    public void setService(TopicService service) {
+        this.service = service;
+    }
+
     @RequestMapping("/topics")
     public List<Topic> getAllTopics() {
-        return Arrays.asList(
-                new Topic("spring","Spring Framework","Spring Framework Description"),
-                new Topic("java","Core Java language","Core java language description"),
-                new Topic("javascript","Core JavaScript language","Core javascript language description")
-        );
+        return getService().getTopics();
     }
 }
